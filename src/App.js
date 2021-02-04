a331bb4065fcdb63991d654d88ce68972f8b3285
Thaís Da Rocha Dal Pozzo <66601480+Thaisdalpozzo@users.noreply.github.com>
2021-02-04 16:24:47 -0300

    Conversor react

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Conversor from "./components/Conversor"
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Conversor de moedas</h1>
+      <h2>Digite o valor no bloco que deseja converter.</h2>
+      <div className="linha">
+        <Conversor moedaA="USD" moedaB="BRL"></Conversor>
+        <Conversor moedaA="BRL" moedaB="USD"></Conversor>
+      </div>
+      <div className="linha">
+        <Conversor moedaA="CAD" moedaB="BRL"></Conversor>
+        <Conversor moedaA="BRL" moedaB="CAD"></Conversor>
+      </div>
+      <div className="linha">
+        <Conversor moedaA="EUR" moedaB="BRL"></Conversor>
+        <Conversor moedaA="BRL" moedaB="EUR"></Conversor>
+      </div>
+    </div>
+  );
+}
+
+export default App;
